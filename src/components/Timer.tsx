321cd8f7a76875ed97d66e8bd1de1676594f395b
tkwbr999 <tkwbr999@gmail.com>
2025-03-15 17:24:10 +0900

    依存関係の更新とTimerコンポーネントの修正

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,21 +1,84 @@
 import { Pause, Play, RefreshCcw } from "lucide-react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { ControlButton } from "./ControlButton";
 import { ModeTypeButton } from "./ModeTypeButton";
+import { TIMER_OPTIONS } from "../constants/index";
 
 const Timer = () => {
   // 作業/休憩モード
   const [mode, setMode] = useState<"work" | "break">("work");
   // タイマーが動いているかどうか
   const [isRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
+  const [pausedTimeRemaining, setPausedTimeRemaining] = useState<number | null>(
+    null
+  );
+
+  const intervalRef = useRef<NodeJS.Timeout | undefined>(undefined);
+
+  function handleStart() {
+    // 後にAudio の設定を追加
+
+    const currentTime = Date.now();
+    // 停止中かつ、１時停止状態の場合
+    if (!isRunning && pausedTimeRemaining) {
+      setStartTime(currentTime - pausedTimeRemaining);
+      setPausedTimeRemaining(null);
+    } else {
+      setStartTime(currentTime);
+    }
+    setNow(Date.now());
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 1000);
+
+    setIsRunning(true);
+  }
+
+  function handleStop() {
+    // インターバル処理の停止
+    clearInterval(intervalRef.current);
+    // 作業中のフラグをfalse
+    setIsRunning(false);
+
+    // 停止された時点の経過時間を、pausedTimeRemainingに格納
+    if (startTime != null && now != null) {
+      // 停止した時点での経過時間
+      setPausedTimeRemaining(now - startTime);
+    }
+  }
+
+  function handleReset() {
+    clearInterval(intervalRef.current);
+    setStartTime(null);
+    setNow(null);
+    setIsRunning(false);
+    setPausedTimeRemaining(null);
+  }
+
+  function handleChangeMode() {
+    handleReset();
+    setMode(mode === "work" ? "break" : "work");
+  }
+
+  // 経過時間(秒)
+  const secondsPassed =
+    startTime != null && now != null ? Math.floor((now - startTime) / 1000) : 0;
+  // 残り時間の秒数（設定時間ー経過時間）
+  const calculateTime = TIMER_OPTIONS[mode].minutes * 60 - secondsPassed;
+  const displayMinutes = Math.floor(calculateTime / 60);
+  const displaySeconds =
+    calculateTime % 60 < 10 ? "0" + (calculateTime % 60) : calculateTime % 60;
 
   // ダミーコード
-  const displayMinutes = "25";
-  const displaySeconds = "00";
-  const handleChangeMode = () => {};
-  const handleStart = () => {};
-  const handleStop = () => {};
-  const handleReset = () => {};
+  //   const displayMinutes = "25";
+  //   const displaySeconds = "00";
+  //   const handleChangeMode = () => {};
+  //   const handleStart = () => {};
+  //   const handleStop = () => {};
+  //   const handleReset = () => {};
 
   return (
     <div
